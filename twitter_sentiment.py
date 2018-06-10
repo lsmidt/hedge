8754daf8bc0fd049dda65fac60dc613229dc7fb6
louis smidt <lsmidt22@gmail.com>
2018-06-10 13:15:20 -0400

    started basic outline of relevant methods, authenticated Twitter

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -13,7 +13,6 @@ Action Items:
     Analyze sentiment of a standard person @ing a company account
     Analyze macroeconomic factors from influential individuals
 
-Twitter Consumer Key
 Analyze Tweets from speficic list of individuals
 Quantify value of tweet content based on number of followers
 Can you find a tweet @ an account?
@@ -22,9 +21,38 @@ dictionaries: positive correlation keywords, negative correlation keywords
 '''
 
 import twitter
-api = twitter.Api(consumer_key='consumer_key',      consumer_secret='consumer_secret',
-                  access_token_key='access_token',  access_token_secret='access_token_secret')
+import requests
+import datetime
+
+api = twitter.Api(consumer_key='zQuVUVHVWNZd7yfMNdyXx4NgJ', consumer_secret='OBMTSJfy4UHuCDSslKzZdcgcm33NChTh1m3dJLX5OhRVY5EhUc',
+    access_token_key='1005588267297853441-aYFOthzthNUwgHUvMJNDCcAMn0IfsC',  access_token_secret='e88p7236E3nrigW1pkvmyA6hUyUWrMDQd2D7ZThbnZvoQ')
+
+
+def get_relevant_tweets(number: int, from_date: datetime) -> list:
+    """
+    RETURN the 'number' most influential tweets after 'from_date'
+    """
+    pass
+
+def find_tweet_sentiment(tweet: str) -> float:
+    """
+    determine the sentiment of a tweet for a specific company
+    """
+    pass
+
+def find_tweet_target(tweet: str) -> str:
+    """
+    determine what company a tweet is most likely talking about.
+    """
+    pass
+
+def get_recent_mentions(account: str) -> list:
+    """
+    find the recent mentions of an account
+    """
+    pass
+
 
 
 status = api.GetUserTimeline('snap')
-#response = GET https://api.twitter.com/1.1/statuses/mentions_timeline.json?count=2&since_id=14927799
+# response = GET https://api.twitter.com/1.1/statuses/mentions_timeline.json?count=2&since_id=14927799
