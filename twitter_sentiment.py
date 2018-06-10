9bee011e8ca067bc4c173117b8f1c99ad9546ead
louis smidt <lsmidt22@gmail.com>
2018-06-10 16:15:11 -0400

    added more method stumps

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -21,38 +21,59 @@ dictionaries: positive correlation keywords, negative correlation keywords
 '''
 
 import twitter
-import requests
-import datetime
-
-api = twitter.Api(consumer_key='zQuVUVHVWNZd7yfMNdyXx4NgJ', consumer_secret='OBMTSJfy4UHuCDSslKzZdcgcm33NChTh1m3dJLX5OhRVY5EhUc',
-    access_token_key='1005588267297853441-aYFOthzthNUwgHUvMJNDCcAMn0IfsC',  access_token_secret='e88p7236E3nrigW1pkvmyA6hUyUWrMDQd2D7ZThbnZvoQ')
+from datetime import date
+import csv
 
 
-def get_relevant_tweets(number: int, from_date: datetime) -> list:
+API = twitter.Api(consumer_key='zQuVUVHVWNZd7yfMNdyXx4NgJ', consumer_secret='OBMTSJfy4UHuCDSslKzZdcgcm33NChTh1m3dJLX5OhRVY5EhUc', access_token_key='1005588267297853441-aYFOthzthNUwgHUvMJNDCcAMn0IfsC', access_token_secret='e88p7236E3nrigW1pkvmyA6hUyUWrMDQd2D7ZThbnZvoQ')
+
+def csv_to_dict_list(csv_file) -> list:
     """
-    RETURN the 'number' most influential tweets after 'from_date'
+    parse csv file into a list of dictionaries
     """
     pass
 
-def find_tweet_sentiment(tweet: str) -> float:
+def get_relevant_tweets(number: int, from_date: date, to_date: date) -> list:
+    """
+    RETURN the 'number' most influential tweets after 'from_date' and before 'to_date'
+    """
+    pass
+
+def find_tweet_sentiment(tweet_text: str) -> float:
     """
     determine the sentiment of a tweet for a specific company
     """
     pass
 
-def find_tweet_target(tweet: str) -> str:
+def find_tweet_target(tweet_text: str) -> str:
     """
     determine what company a tweet is most likely talking about.
+    RETURN str representing company ticker symbol
     """
     pass
 
-def get_recent_mentions(account: str) -> list:
+def get_recent_mentions(account_id: str, number: int) -> list:
     """
-    find the recent mentions of an account
+    find the 'number' most recent mentions of an account
     """
     pass
 
+def shows_purchase_intent(tweet_text) -> bool:
+    """
+    check tweet text for indication that customer purchased product from the target company
+    """
+    pass
+
+def get_account_id_from_name(account_screen_name: str) -> int:
+    """
+    RETURN the numeric id associated with an account
+    """
+    pass
+
+def run_scan(stock_symbol: str)
 
 
-status = api.GetUserTimeline('snap')
+#--------------- Run program ---------------#
+
+STATUS = API.GetUserTimeline('snap')
 # response = GET https://api.twitter.com/1.1/statuses/mentions_timeline.json?count=2&since_id=14927799
