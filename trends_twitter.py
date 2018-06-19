cb7fe901e8501dfe282d9e0ac07b2f61678f6bfe
mgillespie19 <max.gillespie10@gmail.com>
2018-06-19 15:28:22 -0400

    tweet_stream update to program

--- trends_twitter.py
+++ trends_twitter.py
@@ -13,25 +13,46 @@ Max Gillespie
 import nltk
 import tweepy
 
+
+''' ---------------------------- INSTANTIATIONS --------------------------------'''
+CONSUMER_KEY = 'zQuVUVHVWNZd7yfMNdyXx4NgJ'
+CONSUMER_SECRET = 'OBMTSJfy4UHuCDSslKzZdcgcm33NChTh1m3dJLX5OhRVY5EhUc'
+
+auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+
+
+''' ---------------------------- FUNCTIONS ------------------------------------ '''
+def start_tweet_stream(search_terms: list, follow_user_id=None, filter_level="low"):
+    """
+    begin the streaming process. This method blocks the thread until the connection is closed by default
+    """
+    stream_listener = StreamListener()
+    stream = tweepy.Stream(auth, stream_listener)
+
+    printer.pprint("NOW STREAMING")
+    stream.filter(track=search_terms, filter_level=filter_level, languages = ["en"])
+
+
+''' ---------------------------------- MAIN ---------------------------------- '''
 temp_trend_dictionary = dict()
+temp_strings = { "I Love chipotle!" }
 
-s1 = "I Love chipotle!"
+for str in temp_strings:
+    tagged = nltk.tag.pos_tag(str.split(' '))
 
-tagged = nltk.tag.pos_tag(s1.split(' '))
+    for word in tagged:
+        if (word[1] == 'NN'):
+            removals = {'?', ',', '.', '!'}
+            tmp = word[0]
 
-for word in tagged:
-    if (word[1] == 'NN'):
-        removals = {'?', ',', '.', '!'}
-        tmp = word[0]
+            # strip word of any punctuation left from nltk fucking my shit
+            for r in removals:
+                tmp = tmp.replace(r, '')
 
-        # strip word of any punctuation left from nltk fucking my shit
-        for r in removals:
-            tmp = tmp.replace(r, '')
-
-        if not (word[0].lower() in temp_trend_dictionary.keys()):
-            temp_trend_dictionary[ tmp ] = 1
-        else:
-            temp_trend_dictionary[ tmp ]+= 1
+            if not (word[0].lower() in temp_trend_dictionary.keys()):
+                temp_trend_dictionary[ tmp ] = 1
+            else:
+                temp_trend_dictionary[ tmp ]+= 1
 
 
 print (temp_trend_dictionary)
