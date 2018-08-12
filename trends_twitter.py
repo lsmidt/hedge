d3da01cedc3870c2dec93d227815effe5864b944
mgillespie19 <maxgillespie19@gmail.com>
2018-08-12 13:48:45 -0400

    finally FUCKING wOrKIng

--- trends_twitter.py
+++ trends_twitter.py
@@ -47,27 +47,26 @@ punctuation = list(string.punctuation)
 stop = stopwords.words('english') + punctuation + \
         ['rt', 'via', ':', 'https', '@', "'", "''", '...', '’', "'s", "n't", \
         '“', '”', '``', 'de', 'lol', 'haha']
-
+tweets_collected = 0
+most_common_words = []
 
 ''' ----------------------------- CLASSES ----------------------------------'''
 class MyListener(StreamListener):
 
     def on_data(self, data):
         global tweets_collected
-        global j_pos
-        global j_neg
 
         t = json.loads(data)
-        if not filter_tweet(t):
+        if not TwitterTrends.filter_tweet(t):
             return
 
         try:
             # if positive sentiment, write to j_pos
             # else, write to j_neg
-            if (find_tweet_sentiment(t) > 0):
-                j_pos.write(data)
+            if (self.find_tweet_sentiment(t) > 0):
+                TwitterTrends.j_pos.append(data)
             else:
-                j_neg.write(data)
+                TwitterTrends.j_neg.append(data)
 
             tweets_collected += 1
             print(tweets_collected)
@@ -81,7 +80,7 @@ class MyListener(StreamListener):
                     return False
             '''
 
-            if (tweets_collected > 30):
+            if (tweets_collected > 3):
                 return False
 
             return True
@@ -93,119 +92,124 @@ class MyListener(StreamListener):
         print(status)
         return True
 
+    def find_tweet_sentiment(self, tweet) -> float:
+        """
+        determine the sentiment of a tweet for a specific company
+        """
+        if type(tweet) is dict:
+            text = tweet["text"]
+        else:
+            text = tweet.text
+
+        return SIA.polarity_scores(text)["compound"]
 
 
 
-
-''' ---------------------------- FUNCTIONS ---------------------------------'''
-
-
-def sync(topics):
-    global most_common_words
-    global tweets_per_topic
+class TwitterTrends():
+    global most_common_words     # list of most common words to match each topic
+    pos_sentiment = []
+    neg_sentiment = []
+    hash_sentiment = []
+    twitter_stream = Stream(auth, MyListener())
+    j_pos = []
+    j_neg = []
     global tweets_collected
-    global j_pos
-    global j_neg
-    global pos_sentiment
-    global neg_sentiment
-
-    for topic in topics:
-        total_sentiment = 0
-        tweets_collected = 0
-        j_pos = open('positive.json', 'w')
-        j_neg = open('negative.json', 'w')
-
-        # start_time = time.time()
 
 
-        #for t in topic:
-        #    twitter_stream.filter(track=[t])
-
-        twitter_stream.filter(track = topic)
-
-        tweets_per_topic.append(tweets_collected)
-
-        j_pos.close()
-        j_neg.close()
-
-        extra_stop = list()
-        for t in topic:
-            temp = word_tokenize(t.lower())
-
-            if len(temp) == 1:
-                extra_stop.append(temp[0])
-            else:
-                for i in range(0, len(temp)):
-                    extra_stop.append(temp[i])
-
-        tok = tokenize_tweets(extra_stop)
-        most_common_words.append(tok[0])
-
-        pos_sentiment.append(float(tok[1][0]/tok[2][0]))
-        neg_sentiment.append(float(tok[1][1]/tok[2][1]))
-
-        #print ("{}/{}".format(tok[1][0], tok[2][0] ))
-        #print ("{}/{}".format(tok[1][1], tok[2][1] ))
+    def sync(self, topics):
+        for topic in topics:
+            total_sentiment = 0
+            tweets_collected = 0
+            tweets_per_topic = []
 
 
-def find_tweet_sentiment(tweet) -> float:
-    """
-    determine the sentiment of a tweet for a specific company
-    """
-    if type(tweet) is dict:
-        text = tweet["text"]
-    else:
-        text = tweet.text
+            # start_time = time.time()
 
-    return SIA.polarity_scores(text)["compound"]
+            #for t in topic:
+            #    twitter_stream.filter(track=[t])
 
-def tokenize_tweets(extra_stop = []):
-    total_sentiment = [ 0, 0 ]
-    f_pos = open('positive.json', 'r')
-    f_neg = open('negative.json', 'r')
-    files = [ f_pos, f_neg ]
-    file_counter = 0
-    count = list()
-    total_tweets = list()
+            self.twitter_stream.filter(track = topic)
 
-    for f in files:
-        count_all = Counter()
-        total_collected = 0
+            tweets_per_topic.append(tweets_collected)
 
-        for line in f:
-            tweet = json.loads(line) # load it as Python dict
 
-            terms_stop = [term for term in word_tokenize(tweet['text'].lower()) if \
-                                    (term not in stop and term not in extra_stop)]
-            count_all.update(terms_stop)
-            total_sentiment[file_counter] += find_tweet_sentiment(tweet)
-            total_collected += 1
+            extra_stop = list()
+            for t in topic:
+                temp = word_tokenize(t.lower())
 
-        total_tweets.append(total_collected)
-        count.append(count_all.most_common(5))
-        file_counter += 1
+                if len(temp) == 1:
+                    extra_stop.append(temp[0])
+                else:
+                    for i in range(0, len(temp)):
+                        extra_stop.append(temp[i])
 
-    return ([count, total_sentiment, total_tweets])
+            tok = self.tokenize_tweets(extra_stop)
+            most_common_words.append(tok[0])
 
-def filter_tweet(tweet):
-    """
-    filter the tweet from the stream if it is not useful
-    """
+            j_pos = []
+            j_neg = []
 
-    if 'retweeted' in tweet:
-        if tweet['retweeted']:
-            return False
-    if 'friends_count' in tweet:
-        if tweet['friends_count'] < 1000:
-            return False
+            #self.pos_sentiment.append(float(tok[1][0]/tok[2][0]))
+            #self.neg_sentiment.append(float(tok[1][1]/tok[2][1]))
 
-    return True
+            #print ("{}/{}".format(tok[1][0], tok[2][0] ))
+            #print ("{}/{}".format(tok[1][1], tok[2][1] ))
+
+
+    def find_tweet_sentiment(self, tweet) -> float:
+        """
+        determine the sentiment of a tweet for a specific company
+        """
+        if type(tweet) is dict:
+            text = tweet["text"]
+        else:
+            text = tweet.text
+
+        return SIA.polarity_scores(text)["compound"]
+
+    def tokenize_tweets(self, extra_stop = []):
+        total_sentiment = [ 0, 0 ]
+        files = [ self.j_pos, self.j_neg ]
+        file_counter = 0
+        count = list()
+        total_tweets = list()
+
+        for f in files:
+            count_all = Counter()
+            total_collected = 0
+
+            for line in f:
+                tweet = json.loads(line) # load it as Python dict
+
+                terms_stop = [term for term in word_tokenize(tweet['text'].lower()) if \
+                                        (term not in stop and term not in extra_stop)]
+                count_all.update(terms_stop)
+                total_sentiment[file_counter] += self.find_tweet_sentiment(tweet)
+                total_collected += 1
+
+            total_tweets.append(total_collected)
+            count.append(count_all.most_common(5))
+            file_counter += 1
+
+        return ([count, total_sentiment, total_tweets])
+
+    def filter_tweet(tweet):
+        """
+        filter the tweet from the stream if it is not useful
+        """
+
+        if 'retweeted' in tweet:
+            if tweet['retweeted']:
+                return False
+        if 'friends_count' in tweet:
+            if tweet['friends_count'] < 1000:
+                return False
+
+        return True
 
 
 
 ''' ------------------------------ MAIN -----------------------------------'''
-# topics = [ ["Programming", "Python", "Computer Science"], ["Lego"], ]
-# topics = [ ["World Cup", "Mesi"], ["Lego"] ]
 
 '''
 TEST FOR National Beverage Holding Co. (FIZZ)
@@ -216,8 +220,12 @@ topics = [ ["Shasta", "Faygo", "Everfresh", "La Croix", "Rip It", "Clearfruit", 
 
 '''
 
+topics = [ ["World Cup", "Mesi"], ["Lego"] ]
 
+tt = TwitterTrends()
+tt.sync(topics)
 
+'''
 topics = [ [ "bitcoin" ], ["BMW"], ["Shasta", "Faygo", "Everfresh", "La Croix", "Rip It", "Clearfruit", \
             "Mr. Pure", "Ritz", "Crystal Bay", "Cascadia", "Ohana", "Big Shot", \
             "St. Nick's", "Double Hit"] ]
@@ -230,6 +238,12 @@ tweets_per_topic = []
 
 sync(topics)
 
+'''
+
+for i in range(len(topics)):
+    print (most_common_words[i][0])
+
+'''
 # PARSING
 for i in range(len(topics)):
     print ("---------------------------------------------------")
@@ -240,3 +254,4 @@ for i in range(len(topics)):
     print(most_common_words[i][0])
     print("NEG SENTIMENT: ({})".format(neg_sentiment[i]))
     print(most_common_words[i][1])
+'''
