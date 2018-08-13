a2ffc92c126afd4d79ea5151108d6999810c8a41
louis smidt <lsmidt22@gmail.com>
2018-08-13 17:57:11 -0400

    changed some basic shit

--- torch_regression.py
+++ torch_regression.py
@@ -26,10 +26,10 @@ AWS_RDS =  dataset.connect("mysql+pymysql://{}:{}@{}/{}".format\
 input_size = 3 # twitter_sent, headline_sent, wiki_views
 output_size = 1 # composite output
 num_epochs = 300
-learning_rate = 0.002
+learning_rate = 0.02
 
 
-#Data and shit
+#Data and shit 
 x_train = np.array([[450.,80.,14752.],[300.,88.,11000.],[260.,91.,9000.],[496.,98.,1000.],[200.,63.,2000.]],dtype=np.float32)
 y_train = np.array([[3.2],[1.8],[0.2],[1.0],[-1.0]],dtype=np.float32)
 print('x_train:\n',x_train)
@@ -38,13 +38,15 @@ print('y_train:\n',y_train)
 class LinearRegression(nn.Module):
     def __init__(self,input_size,output_size):
         super(LinearRegression,self).__init__()
-        self.linear = nn.Linear(input_size,output_size)
+        self.linear = nn.Linear(input_size,output_size, bias=True)
 
     def forward(self,x):
         out = self.linear(x) #Forward propogation using linear model
         return out
 
 model = LinearRegression(input_size,output_size)
+
+print (*(model.parameters()))
 
 #Lost and Optimizer
 criterion = nn.MSELoss() # using Mean Squared Error loss
@@ -61,10 +63,11 @@ for epoch in range(num_epochs):
     #forward, backward, optimize
     outputs = model(inputs) # generate output from model with all input vectors
     loss = criterion(outputs,targets) #loss function
-    
+    loss.backward()
     
     if(epoch+1) %5 ==0:
         print('epoch [%d/%d], Loss: %.4f' % (epoch +1, num_epochs, loss.data[0]))
+        print (model.linear.weight.data.numpy())
         predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
         plt.plot(x_train,y_train,'ro',label='Original Data')
         plt.plot(x_train,predicted,label='Fitted Line')
@@ -72,7 +75,6 @@ for epoch in range(num_epochs):
         plt.show()
 
     optimizer.zero_grad() # zero the gradients
-    loss.backward() #backward propogation
     optimizer.step() #1-step optimization(gradient descent)
 
         
